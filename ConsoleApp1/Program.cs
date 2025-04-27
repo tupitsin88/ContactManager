20ae5a670715b765b39678fa9b24609466449200
Timothy Tupitsin <timofey08086@gmail.com>
2025-04-27 22:59:01 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Тупицин Тимофей Романович БПИ2410-1 Вариант 6 B-side
+
+using System.Text;
+using Library.Menu;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        private static async Task Main()
+        {
+            // Устанавливаем кодировку консоли.
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            // Запускаем основную логику программы.
+            await InitializeAndRun();
+        }
+
+        /// <summary>
+        /// Инициализирует и запускает основную логику программы.
+        /// </summary>
+        /// <remarks>
+        /// Метод запрашивает путь к файлу с контактами, проверяет его существование,
+        /// считывает данные из файла и передает управление в главное меню.
+        /// </remarks>
+        private static async Task InitializeAndRun()
+        {
+            // Запрашиваем путь к файлу с контактами.
+            Console.Write("Введите путь к файлу с контактами: ");
+            string filePath = Console.ReadLine()!;
+
+            // Проверяем существование файла.
+            while (!File.Exists(filePath))
+            {
+                Console.Write("Файл не существует. Попробуйте снова: ");
+                filePath = Console.ReadLine()!;
+            }
+
+            // Считываем данные из файла.
+            string input = File.ReadAllText(filePath);
+
+            // Запускаем главное меню.
+            await Menu.ShowMainMenu(input, filePath);
+        }
+    }
+}
